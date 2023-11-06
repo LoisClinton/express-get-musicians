4e641fc455d7754cce96f1698f562aa9b0be6eb6
LoisClinton <loisclinton@hotmail.co.uk>
2023-11-06 11:00:29 +0000

    Completed excercises and extensions

--- index.test.js
+++ index.test.js
@@ -1,22 +1,55 @@
 // install dependencies
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+const { execSync } = require("child_process");
+execSync("npm install");
+execSync("npm run seed");
 
-const request = require("supertest")
-const { db } = require('./db/connection');
-const { Musician } = require('./models/index')
-const app = require('./src/app');
+const request = require("supertest"); //allows access to the response to an endpoint.
+const { db } = require("./db/connection");
+const { Musician } = require("./models/index");
+const app = require("./src/app");
 const seedMusician = require("./seedData");
 
+describe("./musicians endpoint", () => {
+  //   A HTTP status code of 200 indicates a successful GET request was made. We can access a status code using the response.statusCode method.
+  test("Testing Musicians endpoint", async () => {
+    // Sends request to `/musicians` endpoint
+    const response = await request(app).get("/musicians");
+    expect(response.statusCode).toBe(200);
+  });
 
-describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
+  //   When sending data from a database the response sends a JSON string. To convert this back to a JSON object where we can access values, we can use JSON.parse() on the text in the response.
+  test("Testing Musicians endpoint has the right entries", async () => {
+    const response = await request(app).get("/musicians");
+    const responseData = JSON.parse(response.text);
+    expect(responseData[0].name).toBe("Mick Jagger");
+  });
+  test("Testing Musicians endpoint is made up of objects", async () => {
+    const response = await request(app).get("/musicians");
+    const responseData = JSON.parse(response.text);
 
+    expect(typeof responseData[0]).toBe("object");
+  });
+});
 
+describe("./bands endpoint", () => {
+  //   A HTTP status code of 200 indicates a successful GET request was made. We can access a status code using the response.statusCode method.
+  test("Testing bands endpoint", async () => {
+    // Sends request to `/bands` endpoint
+    const response = await request(app).get("/bands");
+    expect(response.statusCode).toBe(200);
+  });
 
+  //   When sending data from a database the response sends a JSON string. To convert this back to a JSON object where we can access values, we can use JSON.parse() on the text in the response.
+  test("Testing bands endpoint has the right entries", async () => {
+    const response = await request(app).get("/bands");
+    const responseData = JSON.parse(response.text);
+    expect(responseData[0].name).toBe("The Beatles");
+  });
 
-    
-})
+  test("Testing bands endpoint is made up of objects", async () => {
+    const response = await request(app).get("/bands");
+    const responseData = JSON.parse(response.text);
+
+    expect(typeof responseData[0]).toBe("object");
+  });
+});
--- src/app.js
+++ src/app.js
@@ -1,16 +1,38 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
-const { db } = require("../db/connection")
+const { Musician } = require("../models/index");
+const { Band } = require("../models/index");
+const { db } = require("../db/connection");
 
 const port = 3000;
 
-//TODO: Create a GET /musicians route to return all musicians 
+//Create a GET /musicians route to return all musicians
+app.get("/musicians", async (req, res) => {
+  const musicians = await Musician.findAll();
+  // Send musicians as converted to a JSON string .
+  res.json(musicians);
+});
 
+app.get("/musicians/1", async (req, res) => {
+  const musiciansOne = await Musician.findByPk(1);
+  res.json(musiciansOne);
+});
 
+app.get("/musicians/2", async (req, res) => {
+  const musiciansTwo = await Musician.findByPk(2);
+  res.json(musiciansTwo);
+});
 
+app.get("/musicians/3", async (req, res) => {
+  const musiciansThree = await Musician.findByPk(3);
+  res.json(musiciansThree);
+});
 
-
-
+//Create a GET /bands route to return all bands.
+app.get("/bands", async (req, res) => {
+  const bands = await Band.findAll();
+  // Send bands as converted to a JSON string .
+  res.json(bands);
+});
 
 module.exports = app;
